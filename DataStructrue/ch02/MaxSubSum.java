2f68fd4577ca6e8ae066c44d7e3527edbf208365
laoheihei <studylalala@163.com>
2017-03-29 00:01:39 +0800

    add O(NlogN) & O(N)

--- DataStructrue/ch02/MaxSubSum.java
+++ DataStructrue/ch02/MaxSubSum.java
@@ -1,8 +1,11 @@
 //P26最大子序列和问题
 public class MaxSubSum {
+    
     private MaxSubSum() {}
     
+    /*-----------------O(n^2)-------------------*/
     public static int maxSubSum1(int[] a) {
+        
         int maxSum = 0;
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -16,4 +19,85 @@ public class MaxSubSum {
         
         return maxSum;
     }
+    
+    /*-----------------O(NlogN)-------------------*/
+    public static int maxSubSum2(int[] a) {
+        
+        return maxSubMax2(a, 0, a.length - 1);
+    }
+    //求[lo, hi]的最大子序列
+    private static int maxSubSum2(int[] a, int lo, int hi) {
+        
+        if (hi < lo + 1)
+            return (0 < a[lo]) ? a[lo] : 0;
+        
+        int mi = (lo + hi) / 2;        
+        leftMax = maxSubSum2(a, lo, mi);
+        rightMax = maxSubSum2(a, mi + 1, hi);
+        
+        int maxLeftBranch = 0,
+            sumLeftBranch = 0,
+            maxRightBranch = 0,
+            sumRightBranch = 0;
+            
+        for (int i = mi; i >= lo; i--) {
+            sumLeftBranch += a[i];
+            if (maxLeftBranch < sumLeftBranch)
+                maxLeftBranch = sumLeftBranch;
+        }
+        for (int i = mi + 1; i <= hi; i++) {
+            sumRightBranch +=a[i];
+            if (maxRightBranch < sumRightBranch)
+                maxRightBranch = sumRightBranch;
+        }
+        
+        int branchMax = maxRightBranch + maxLeftBranch;
+        
+        return max(leftMax, rightMax, branchMax);
+    }
+    private static int max(int a, int b, int c) {
+        int tempMax = 0;
+        tempMax = a < b ? b : a;
+        tempMax = c < tempMax ? tempMax : c;
+        return tempMax;
+    }
+    
+    /*-----------------------O(n)----------------*/
+    public static int maxSubSum(int[] a) {
+        int maxSum = 0;
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+            if (maxSum < sum)
+                maxSum = sum;
+            if (sum < 0)
+                sum = 0;
+        }
+        
+        return maxSum;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
